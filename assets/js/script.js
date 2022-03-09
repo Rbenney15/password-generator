970755ed43406ed37a51c57ec98c33b48b2b980d
Rob Benney <rob.benney@gmail.com>
2022-03-08 21:54:10 -0500

    generate random lower and upper case letter, random numbers, random symbols

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,21 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Generate random lower, upper, number, and symbol functions
+function getRandomLower () {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper () {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  var symbols = "`!@#$%^&*(){}[]=<>/,.'";
+  return symbols [Math.floor(Math.random() * symbols.length)];
+}
